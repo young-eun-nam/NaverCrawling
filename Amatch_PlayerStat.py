acf7d668f25e1496dcddf36950f0af44a54d970f
zeroistwo <jon061512@gmail.com>
2018-11-07 11:05:39 +0900

    Amatch Stat Update

--- Amatch_PlayerStat.py
+++ Amatch_PlayerStat.py
@@ -13,6 +13,10 @@ YEARUIL = "&year="
 YEAR = YEARUIL + year
 MONTH = "&month="
 
+# CLASS 명
+HOMEBUTTON = "home_team_btn"
+AWAYBUTTON = "away_team_btn"
+
 # 기타 변수
 FILEROUTE = '/Users/admin/chromedriver'
 DATAFRAME = ['Date', 'Day', 'Stadium', 'Team', 'Position', 'Back_Number', 'Name', 'Substitute', 'Goal', 'Assist', 'Shoot', 'Foul', 'Yellow_Card', 'Sending_Off', 'Corner_Kick', 'Off-side']
@@ -20,7 +24,7 @@ FILENAME = "Naver_PlayerStat"
 league_str = "Amatch"
 
 def setDriver(url, driver):
-    driver. get(url)
+    driver.get(url)
     page = driver.page_source
     soup = bs(page, 'html.parser')  # Selenium driver로 가져온 페이지를 읽기 위해 BeautifulSoup사용하기
     return soup
@@ -29,24 +33,28 @@ def setBasicInfo():
     league_url = URL + CATEGORY
     driver = webdriver.Chrome(FILEROUTE)
     schedule_soup = setDriver(league_url, driver)
-    recording_link = schedule_soup.select('div.inner > a')    # 경기 기록 버튼 bs로 찾기
-    each_game_stat_list = getData(recording_link, driver)     # getData 함수를 통해 crawling
+    recording_link = schedule_soup.select('div.inner > a')  # 경기 기록 버튼 bs로 찾기
+    each_game_stat_list = getData(recording_link, driver)  # getData 함수를 통해 crawling
     driver.close()
     return each_game_stat_list
 
-def getStat(recording_soup, d_day, day, stadium):
+def getStat(BUTTON, driver, recording_soup, d_day, day, stadium):
     stat_list = []
-    player_record_box = recording_soup.find('tbody', id='player_record_box').findAll('tr')
+    driver.find_element_by_css_selector('#player_stats_btn').click()
+    team_name = recording_soup.find('a', id=BUTTON).span.get_text()
+    driver.find_element_by_css_selector('#' + BUTTON).click()
+    # Home/Away 버튼을 클릭함으로써 page_source가 달라졌기 때문에 html.parser를 다시 불러와서 page source를 읽음.
+    btn_change_soup = bs(driver.page_source, 'html.parser')
+    player_record_box = btn_change_soup.find('tbody', id='player_record_box').findAll('tr')
     for each_player in range(len(player_record_box)):
         raw = player_record_box[each_player].findAll('td')
-        get_team_name = recording_soup.findAll('strong', class_='team_name')
         # 교체된 선수인 경우 선수 이름 옆에 OUT ICON이 함께 크롤링 되기 때문에 확인
         is_exist_exchange = player_record_box[each_player].find('td', class_='name').span
         raw_data = []
-        raw_data.append(d_day)                          # 1. Date
-        raw_data.append(day)                            # 2. Day
-        raw_data.append(stadium)                        # 3. Stadium
-        raw_data.append(get_team_name[0].get_text())    # 4. Team
+        raw_data.append(d_day)  # 1. Date
+        raw_data.append(day)  # 2. Day
+        raw_data.append(stadium)  # 3. Stadium
+        raw_data.append(team_name)  # 4. Team
         # 5. Position ~ # 16. Off-Side
         for each_column in range(len(raw)):
             if each_column == 2 and is_exist_exchange:
@@ -69,8 +77,10 @@ def getData(recording_link, driver):
             day = get_date_info.split('.')[1].split('(')[1].split(')')[0]
             d_day = datetime.date(int(year), int(month), int(date))
             stadium = get_date_info.split(') ')[1]
-            stat = getStat(recording_soup, d_day, day, stadium)
-            each_month_stat_list.extend(stat)
+            home_team_stat = getStat(HOMEBUTTON, driver, recording_soup, d_day, day, stadium)
+            away_team_stat = getStat(AWAYBUTTON, driver, recording_soup, d_day, day, stadium)
+            team_stat = home_team_stat + away_team_stat
+            each_month_stat_list.extend(team_stat)
     return each_month_stat_list
 
 def checkPlyerBackNumber(get_squad_name):
