feb93b44087c8199dc84528ed95e48fe789e56ed
zeroistwo <jon061512@gmail.com>
2019-01-21 18:44:04 +0900

    Server Check 추가

--- NaverRecord.py
+++ NaverRecord.py
@@ -44,7 +44,14 @@ def getData(record_soup, class_name):
 
 def setBasicInfo(league_num, league_str):
     record_url = URL + CATEGORY + league_str + YEAR
-    driver = webdriver.Chrome(FILEROUTE)
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    browser = webdriver.Chrome(chrome_options=chrome_options)
+
+    driver = webdriver.Chrome()
     record_soup = setDriver(record_url, driver)
     if league_num in ["1"]:
         k1_groupA_result = getData(record_soup, groupA)
